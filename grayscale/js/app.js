fc104f1ec09e4e68cccdd7e1d24b720c4433d90e
fjasonlin <fjasonlin@gmail.com>
2014-08-05 06:10:03 +0000

    update

--- grayscale/js/app.js
+++ grayscale/js/app.js
@@ -45,97 +45,51 @@ app.Post = Backbone.Model.extend({
     defaults: {
         "errors": [],
         "errfor": {},
-
         "post": {
-
             "_id": "53d4b7e28ceee0942f000957",
-
             "__v": 0,
-
             "userCreated": {
-
                 "id": "530223ee98b4052056000009",
-
                 "time": "2014-07-27T08:27:14.832Z",
-
                 "name": "jollenchen"
-
             },
-
             "wchars": 0,
-
             "date": "2014-07-27T08:27:14.832Z",
-
             "isActive": true,
-
             "tags": [],
-
             "html": "",
-
             "content": "* Backbone Model\n * url\n * save, fetch\n* Underscore (Template)\n* Backbone View\n * initialize()\n * render()",
-
             "subject": "學習重點"
-
         }
-
     }
-
 });
-
-
-
 
 
 app.PostCollection = Backbone.Collection.extend({
-
     model: app.Post
-
 });
 
 
-
-
-
 /**
-
 * VIEWS
-
 **/
-
 app.PostItemView = Backbone.View.extend({
-
     el: '#postitems',
-
     initialize: function() {
-
         var self = this;
 
-        
-
 		this.model = new app.PostItem();
-
         this.collection = new app.PostCollection();
 
-        
-
         this.template = _.template($('#tmpl-postitem').html());
-
         this.model.bind('change', this.render, this);
 
-        
-
         this.model.fetch({
-
             success: function(model, response, options) {	
-
-                // FIXME:
-
+                self.prefetch(); 
                 $('#status-loading').addClass('hide');
-
             }
-
         }); 
-
     },
 
     prefetch: function() {
@@ -145,44 +99,25 @@ app.PostItemView = Backbone.View.extend({
             var me = $(this),
                 id = me.data('post-id'),
                 post = new app.Post({ id: id});
-            
             //post('id', id);
             post.fetch();
-            
             self.collection.push(post);
-            
             console.log(JSON.stringify(post.attributes));
         });
-        
-
     },
-
     render: function() {
-
  		var htmlCodes = this.template(this.model.attributes);
-
         this.$el.html(htmlCodes);
-
     }
-
 });
 
-
-
 app.PostView = Backbone.View.extend({
-
     el: '#post',
-
     initialize: function() {
-
 		this.model = new app.Post();
-
         this.template = _.template($('#tmpl-post').html());
-
         this.model.bind('change', this.render, this);
-
     },
-
     render: function() {
  		var htmlCodes = this.template(this.model.attributes);
         this.$el.html(htmlCodes);
@@ -194,5 +129,3 @@ $(document).ready(function(){
     app.postItemView = new app.PostItemView();
     app.postView = new app.PostView();
 });
-
-
